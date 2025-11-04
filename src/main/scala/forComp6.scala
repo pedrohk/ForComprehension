1235ff917477a9a4b58f379ec80bf48315f9c2d8
Pedro <pedrohk@gmail.com>
2025-11-04 00:35:59 -0300

    Update forComp6.scala

--- src/main/scala/forComp6.scala
+++ src/main/scala/forComp6.scala
@@ -4,6 +4,7 @@ sealed trait SResult[+E, +A]
 case class SRight[A](value: A) extends SResult[Nothing, A]
 
 case class SLeft[E](error: E) extends SResult[E, Nothing]
+
 object forComp6 {
 
 
@@ -21,8 +22,17 @@ object forComp6 {
 
   def compute(x: Int, y: Int): SResult[Throwable, Int] = {
     for {
-      a <- if (x > 0) SRight(x * 2) else SLeft(new IllegalArgumentException("x must be positive"))
-      b <- if (y != 0) SRight(y + 10) else SLeft(new ArithmeticException("y cannot be zero"))
+      a <- if (x > 0) {
+        SRight(x * 2)
+      } else {
+        SLeft(new IllegalArgumentException("x must be positive"))
+      }
+
+      b <- if (y != 0) {
+        SRight(y + 10)
+      } else {
+        SLeft(new ArithmeticException("y cannot be zero"))
+      }
       result <- SRight(a / y)
     } yield result
   }
